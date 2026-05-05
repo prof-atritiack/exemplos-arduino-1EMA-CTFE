31de780748e4255807448533c2f921c73132f0c5
prof-atritiack <atritiack@gmail.com>
2026-05-05 09:50:40 -0300

    Atualizado para botão ON

--- Exemplo_Edges_Arduino/Exemplo_Edges_Arduino.ino
+++ Exemplo_Edges_Arduino/Exemplo_Edges_Arduino.ino
@@ -4,6 +4,9 @@
 #define BT_E 9
 #define BT_D 10
 
+bool statusBT_D, statusBT_E;
+bool flagD, flagE;
+
 void setup() {
   Serial.begin(9600);
   pinMode(LED_1, OUTPUT);
@@ -14,10 +17,34 @@ void setup() {
 }
 
 void loop() {
+  statusBT_E = digitalRead(BT_E);
+  statusBT_D = digitalRead(BT_D);
+  delay(50);
+
   // Para o botão da direita
   // Detectar a transição negativa do sinal (falling edge ou borda de descida)
   // Quando o sinal passa de HIGH (botão OFF) para LOW (botão ON)
   // Apenas imprimir no Serial Monitor que houve a transição
+  if(statusBT_D == LOW && flagD == LOW){
+    Serial.println("Borda de descida detectada!!! :) ");
+    flagD = HIGH;
+  }
+    // Para o botão da direita
+  // Detectar a transição negativa do sinal (falling edge ou borda de descida)
+  // Quando o sinal passa de HIGH (botão OFF) para LOW (botão ON)
+  // Apenas imprimir no Serial Monitor que houve a transição
+    if(statusBT_D == HIGH && flagD == HIGH){
+    Serial.println("Borda de subida detectada!!! :) ");
+    flagD = LOW;
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, HIGH);
+  }
+
+
+  
+
+
 
   // Detectar a transição positiva do sinal (rising edge ou borda de subida)
   // Quando o sinal passa de LOW (botão ON) para HIGH (botão OFF)
